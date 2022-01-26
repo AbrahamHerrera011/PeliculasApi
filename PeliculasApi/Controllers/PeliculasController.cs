2ea8b4890a70478bfc8ba0bf440b1fb1e06280db
AbrahamHerrera011 <92007619+AbrahamHerrera011@users.noreply.github.com>
2022-01-26 11:40:56 -0600

    Add files via upload

---
+++ PeliculasApi/Controllers/PeliculasController.cs
@@ -0,0 +1,121 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using PeliculasApi.Models;
+using PeliculasApi.Controllers;
+using Microsoft.EntityFrameworkCore;
+
+namespace PeliculasApi.Controllers
+{
+    [Route("api")]
+    [ApiController]
+    public class PeliculasController : ControllerBase
+    {
+        private readonly PeliculasContext contexto;
+        
+
+        public PeliculasController(PeliculasContext _contexto)
+        {
+            contexto = _contexto;
+        }
+
+        [HttpGet]
+        [Route("films")]
+        public async Task<IQueryable<Pelicula>> GetAll()
+        {
+            var query = await contexto.Peliculas.AsQueryable<Pelicula>().AsNoTracking().ToListAsync();
+            return query.AsQueryable();
+        }
+
+
+        [HttpGet]
+        [Route("films/{id:int}")]
+        public async Task<Pelicula> ObtenerporID(int id)
+        {
+            var query = await contexto.Peliculas.FirstOrDefaultAsync(x => x.Idpeliculas == id);
+            return query;
+        }
+
+        [HttpPost]
+        [Route("films")]
+        public async Task<int> crear(Pelicula Nuevapelicula)
+        {
+            var query = Nuevapelicula;
+            await contexto.AddAsync(query);
+            var guardar = await contexto.SaveChangesAsync();
+            return query.Idpeliculas;
+
+        }
+
+        [HttpPut]
+        [Route("films/{id:int}")]
+        public async Task<bool> actualizar(int id, Pelicula NuevaPelicula)
+        {
+            var query = await ObtenerporID(id);
+            query.Titulo = NuevaPelicula.Titulo;
+            query.Director = NuevaPelicula.Director;
+            query.Genero = NuevaPelicula.Genero;
+            query.Puntacion = NuevaPelicula.Puntacion;
+            query.Rating = NuevaPelicula.Rating;
+            query.AnioDePublicacion = NuevaPelicula.AnioDePublicacion;
+            contexto.Update(query);
+            var guardar = await contexto.SaveChangesAsync();
+            return guardar > 0;
+
+        }
+        [HttpDelete]
+        [Route("films/{id:int}")]
+        public async Task<bool> borrar(int id)
+        {
+            var query = await ObtenerporID(id);
+            contexto.Remove(query);
+            var guardar = await contexto.SaveChangesAsync();
+            return guardar > 0;
+        }
+
+
+        [HttpGet]
+        [Route("films/filters")]
+        public async Task<IQueryable<Pelicula>> ObtenerporFiltro([FromBody] Pelicula buscarPeliculaNombre)
+        {
+            if (buscarPeliculaNombre == null)
+            {
+                return new List<Pelicula>().AsQueryable();
+            }
+            var query = contexto.Peliculas.AsQueryable();
+
+
+            if (!string.IsNullOrEmpty(buscarPeliculaNombre.Titulo))
+            {
+                query = query.Where(x => x.Titulo.Contains(buscarPeliculaNombre.Titulo));
+            }
+
+            if (!string.IsNullOrEmpty(buscarPeliculaNombre.Director))
+            {
+                query = query.Where(x => x.Director.Contains(buscarPeliculaNombre.Director));
+            }
+            if (!string.IsNullOrEmpty(buscarPeliculaNombre.Genero))
+            {
+                query = query.Where(x => x.Genero.Contains(buscarPeliculaNombre.Genero));
+            }
+
+            if (!string.IsNullOrEmpty(buscarPeliculaNombre.Rating))
+            {
+                query = query.Where(x => x.Rating.Contains(buscarPeliculaNombre.Rating));
+            }
+
+            var resultado = await query.ToListAsync();
+            return resultado.AsQueryable().AsNoTracking();
+        }
+
+
+
+
+
+
+    }
+
+}
---
+++ PeliculasApi/Migrations/20220126170439_MigracionInicial.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PeliculasApi.Migrations
+{
+    public partial class MigracionInicial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Peliculas",
+                columns: table => new
+                {
+                    IDpeliculas = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Titulo = table.Column<string>(type: "varchar(50)", unicode: false, maxLength: 50, nullable: false),
+                    Director = table.Column<string>(type: "varchar(60)", unicode: false, maxLength: 60, nullable: false),
+                    Genero = table.Column<string>(type: "varchar(30)", unicode: false, maxLength: 30, nullable: false),
+                    puntacion = table.Column<double>(type: "float", nullable: false),
+                    Rating = table.Column<string>(type: "varchar(40)", unicode: false, maxLength: 40, nullable: false),
+                    Anio_de_publicacion = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_Idpeliculas", x => x.IDpeliculas);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Peliculas");
+        }
+    }
+}
